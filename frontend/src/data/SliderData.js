21a8745aadbfbc391620339c19889e100e436553
Derick1530 <derickzihalirwa07@gmail.com>
2021-11-11 23:45:53 +0300

    Update SliderData.js

--- frontend/src/data/SliderData.js
+++ frontend/src/data/SliderData.js
@@ -8,20 +8,24 @@ export const SliderData = [
         name: "Urban Design",
         to: "/galery",
         image: ImageOne,
+        title: "Biden’s Expansion Of Student Loan Forgiveness Program For Public Service Workers: A Breakdown"
     },
     {
         name: "Interior Design",
         to: "/galery",
         image: ImageTwo,
+        title: "Biden’s Expansion Of Student Loan Forgiveness Program For Public Service Workers: A Breakdown"
     },
     {
         name: "Branding",
         to: "/galery",
         image: ImageThree,
+        title: "Biden’s Expansion Of Student Loan Forgiveness Program For Public Service Workers: A Breakdown"
     },
     {
         name: "Landscape",
         to: "/galery",
         image: ImageFour,
+        title: "Biden’s Expansion Of Student Loan Forgiveness Program For Public Service Workers: A Breakdown"
     },
 ];
